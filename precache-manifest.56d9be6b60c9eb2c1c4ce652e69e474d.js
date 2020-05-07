c66fa5af94957c49824b1c7a5d5fac0f0177e344
Malik296 <malik.khasammadov@gmail.com>
2020-05-07 17:29:34 +0400

    Updates

---
+++ precache-manifest.56d9be6b60c9eb2c1c4ce652e69e474d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b792a8e7b9dfbe425d2d3262d2401bc3",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "2495b479966f4c6e10e8",
+    "url": "/my-app/static/css/2.8d06fd47.chunk.css"
+  },
+  {
+    "revision": "4ca636e454817fd778ab",
+    "url": "/my-app/static/css/main.a0380d73.chunk.css"
+  },
+  {
+    "revision": "2495b479966f4c6e10e8",
+    "url": "/my-app/static/js/2.ac575b51.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/my-app/static/js/2.ac575b51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ca636e454817fd778ab",
+    "url": "/my-app/static/js/main.2de4827d.chunk.js"
+  },
+  {
+    "revision": "6b90711d51ae396ac389",
+    "url": "/my-app/static/js/runtime-main.8c451e69.js"
+  },
+  {
+    "revision": "aba9de2d23eff684aab3d2d4bbde5f30",
+    "url": "/my-app/static/media/ui.aba9de2d.png"
+  }
+]);
